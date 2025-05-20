60abe10f8bac65aab4e97c1f68fbe14e11391188
subhamsam007 <mondalsubham801@gmail.com>
2025-05-21 02:12:56 +0530

    notify & hook validation

--- src/Components/Create.jsx
+++ src/Components/Create.jsx
@@ -1,30 +1,33 @@
 import React, { use } from 'react'
 import { useState } from 'react'
 import {nanoid} from 'nanoid'
+import { useForm } from "react-hook-form"
+import {ToastContainer, toast} from 'react-toastify'
 
 function Create(props) {
-  const info = props.info
+  const info = props.info 
   const setInfo = props.setInfo
 
-  const [task, setTask] = useState("")
+  // const [task, setTask] = useState("")
+const {register, handleSubmit, reset,formState:{errors}} = useForm()
 
-  const createTodo = (e) => {
-    e.preventDefault()
-    const newData = {
-      id: nanoid(),
-      task: task,
-      completed: false
-    }
-    setInfo([...info, newData])
-    setTask("")
+  const createTodo = (data) => {
+    data.id=nanoid()
+    data.task=data.title
+    data.status=false
+    setInfo([...info, data])
+    toast.success("Task created successfully");
+    reset();
   }
 
 
   return (
     <div className='w-1/2 bg-indigo-300 h-screen text-2xl text-black p-5 border-4 ' >
+      <ToastContainer position='top-center' autoClose={2000} />
       <h1>set the <span className='font-bold text-red-400 underline  decoration-black decoration-1 underline-offset-8'>the task u want </span> to-do</h1>
-      <form onSubmit={(e) => { createTodo(e) } } className='flex flex-col items-center justify-center mt-10'>
-        <input type="text" placeholder='task' onChange={(e) => { setTask(e.target.value) } } value={task} />
+      <form onSubmit={handleSubmit(createTodo)} className='flex flex-col items-center justify-center mt-10'>
+        <input type="text" placeholder='task' {...register("title",{required:"task cant be empty"})} />
+        <span className='text-red-500'>{errors.title?.message}</span>
         <br />
         <br />
         <button type="submit">Create</button>
