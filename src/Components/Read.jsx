6c960d177ad5d045e32f44bef3248672b549c794
subhamsam007 <mondalsubham801@gmail.com>
2025-05-23 02:29:02 +0530

    style

--- src/Components/Read.jsx
+++ src/Components/Read.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {toast} from 'react-toastify'
 
 const Read = (props) => {
   const info = props.info
@@ -8,14 +9,16 @@ const Read = (props) => {
     const dedata = info.filter((task) => {
       return task.id !== id
     })
+    toast.error("Task deleted successfully");
     setInfo(dedata)
   }
 
   const renderuserr = info.map((task, index) => {
     return (
-      <li key={index}>
-        {task.task}
-        <button onClick={() => deletetask(task.id)}>
+      <li className='border-2 mt-4  border-gray-500 rounded-lg h-20 w-full flex items-center justify-between ' key={index} >
+        {task.task} 
+        <button className='bg-red-500 text-white  w-[40%] rounded-lg px-2 py-2 ' onClick={() => deletetask(task.id)}>
+
           delete
         </button>
       </li>
@@ -23,7 +26,9 @@ const Read = (props) => {
   })
 
   return (
-    <div>
+    <div className='border-4 border-amber-300  w-[30%] mt-10 p-5  '>
+      <h1 className='text-6xl text-pink-500 font-extralight '>Pendeing</h1>
+      <br />
       <ul>
         {renderuserr}
       </ul>
